5163d0afcb874feb895f3dacf3d4d5b5534bd479
Ahmadreza <lovlyboy@gmail.com>
2024-04-30 07:14:50 +0330

    Initial Commit

---
+++ Bricks Shooter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bricks Shooter
+//
+//  Created by Ahmadreza on 4/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bricks ShooterUITests/Bricks_ShooterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Bricks_ShooterUITestsLaunchTests.swift
+//  Bricks ShooterUITests
+//
+//  Created by Ahmadreza on 4/30/24.
+//
+
+import XCTest
+
+final class Bricks_ShooterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
